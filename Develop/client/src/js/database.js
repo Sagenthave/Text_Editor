3aeb34c42a1f21b89c14919aedabfacbeaefc5c1
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-08-01 12:45:08 -0400

    added logic to a method that accepts some content and adds it to the database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,10 +14,30 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("PUT to the database")
-  console.error('putDb not implemented');
+  console.log('Post to the database');
+
+  const textDb = await openDB ("jare", 1);
+  const tx = textDb.transaction("jate", "readWrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+
+  console.log('🚀 - data saved to the database', result);
+  // console.error('putDb not implemented');
 }
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB("jate", 1);
+  const tx = contactDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+
+  console.log("result.value", result);
+  return result?.value;
+};
+// console.error('getDb not implemented');
 
 initdb();
